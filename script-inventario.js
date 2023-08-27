e1f151497521395d5b8c51052461edf53af66b8f
Emerson Almeida <138174496+emersonszalmeida@users.noreply.github.com>
2023-08-26 23:27:21 -0300

    adicionei menu

--- script-inventario.js
+++ script-inventario.js
@@ -8,6 +8,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const linhasTintas = tabelaTintas.getElementsByTagName('tr');
     const inputFiltrar = document.getElementById('filtrar-tinta');
     const checkboxMarcarDesmarcarTodos = document.getElementById('marcar-desmarcar-todos');
+    const menuIcon = document.getElementById("menu-icon");
+    const menuLinks = document.getElementById("menu-links");
+
+    menuIcon.addEventListener("click", function () {
+        menuLinks.classList.toggle("active");
+        menuIcon.classList.toggle("active");
+    });
 
     checkboxMarcarDesmarcarTodos.addEventListener('change', function () {
         checkboxes.forEach(function (checkbox) {
